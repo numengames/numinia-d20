314516000ac0938172e780e755260c28edd4a3e5
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-05-13 14:12:26 +0200

    change host to localhost instead of 0.0.0.0

--- d20_governance/health_check.py
+++ d20_governance/health_check.py
@@ -10,6 +10,6 @@ async def health_check():
     return Response(status_code=200)
 
 async def start_server():
-    config = uvicorn.Config(app=app, host="0.0.0.0", port=8000, loop="asyncio")
+    config = uvicorn.Config(app=app, host="localhost", port=8000, loop="asyncio")
     server = uvicorn.Server(config)
     await server.serve()
